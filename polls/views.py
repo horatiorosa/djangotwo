b54a32d1d688e372f594fd91b8104a818193ca27
Horatio <horatiorosa@aol.com>
2015-10-31 22:31:44 -0400

    modified URLs and results.html

--- polls/views.py
+++ polls/views.py
@@ -1,45 +1,62 @@
 # from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect #, HttpResponse
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 # from django.template import RequestContext, loader
 
 from . models import Choice, Question
 
-# Create your views here.
+# def index(request):
+# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+# 	# template = loader.get_template('polls/index.html')
+# 	# context = RequestContext(request, {
+# 	# 	"latest_question_list": latest_question_list,
+# 	# 	})
+# 	context = {"latest_question_list": latest_question_list}  # shorthand way of writing above commented out lines
+# 	return render(request, 'polls/index.html', context)
+# 	# return HttpResponse(template.render(context))  # not needed with shortcut syntax
+# 	# output = ', '.join([p.question_date for p in latest_question_list])
+# 	# return HttpResponse(output)
+
+##### the above written as Django generic views ######
 
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	# template = loader.get_template('polls/index.html')
-	# context = RequestContext(request, {
-	# 	"latest_question_list": latest_question_list,
-	# 	})
-	context = {"latest_question_list": latest_question_list}  # shorthand way of writing above commented out lines
-	return render(request, 'polls/index.html', context)
-	# return HttpResponse(template.render(context))  # not needed with shortcut syntax
-	# output = ', '.join([p.question_date for p in latest_question_list])
-	# return HttpResponse(output)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		""" Return the last five published questions """
+		return Question.objects.order_by('-pub_date')[:5]
+
+# def detail(request, question_id):
+# 	# try:
+# 		# question = Question.objects.get(pk=question_id)
+# 	# except Question.DoesNotExist:
+# 	# 	raise Http404("Question does not exist")
+# 	#shortcut method below
+# 	question = get_object_or_404(Question, pk=question_id)  # django model passed as 1st arguement and arbitrary number of keyword arguments, which it passes to the get() and 404 is the object doesnt exist
+
+##### the above written as Django generic views ######
 
 
-def detail(request, question_id):
-	# try:
-		# question = Question.objects.get(pk=question_id)
-	# except Question.DoesNotExist:
-	# 	raise Http404("Question does not exist")
-	#shortcut method below
-	question = get_object_or_404(Question, pk=question_id)  # django model passed as 1st arguement and arbitrary number of keyword arguments, which it passes to teh get() and 404 is the object doesnt exist
-	# return HttpResponse("You're looking at question %s." % question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	retun render(request, 'polls:results.html', {'question': question})
+# def results(request, question_id):
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	return render(request, 'polls/results.html', {'question': question})
 
-	response = "You're looking at the results of a question %s."
-	return HttpResponse(response % question_id)
+	##### the above written as Django generic views ######
+
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -58,5 +75,4 @@ def vote(request, question_id):
 			# if a user hits the Back button
 			return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
-	# return HttpResponse("You're voting on a question %s." % question_id)
 
